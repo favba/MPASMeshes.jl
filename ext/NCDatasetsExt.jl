4774451c4bd195e4853697e4d0e146165f878713
Felipe A. V. de Bragança Alves <favbalves@gmail.com>
2024-12-03 10:24:33 -0300

    `MPASMesh` Implementation.

---
+++ ext/NCDatasetsExt.jl
@@ -0,0 +1,120 @@
+module NCDatasetsExt
+
+using MPASMeshes, VoronoiOperators, NCDatasets, TensorsLite, Zeros, ImmutableVectors
+import MPASMeshes: VoronoiMeshes
+import VoronoiMeshes: copy_matrix_to_tuple_vector!, save_to_netcdf!
+
+function read_tanVelRecon(::Val{maxEdgesOnEdges}, nEdges::AbstractVector, ncfile) where {maxEdgesOnEdges}
+    n = length(nEdges)
+    inds_tuple = Vector{NTuple{maxEdgesOnEdges, Int32}}(undef, n)
+    inds = ImmutableVectorArray(inds_tuple, nEdges)
+    edgesOnEdgeArray = ncfile["edgesOnEdge"][:,:]::Matrix{Int32}
+    copy_matrix_to_tuple_vector!(inds_tuple, edgesOnEdgeArray)
+
+    w_tuple = Vector{NTuple{maxEdgesOnEdges, Float64}}(undef, n)
+    w = ImmutableVectorArray(w_tuple, nEdges)
+    weightsArray = ncfile["weightsOnEdge"][:,:]::Matrix{Float64}
+    copy_matrix_to_tuple_vector!(w_tuple, weightsArray)
+
+    return TangentialVelocityReconstructionThuburn(inds, w)
+end
+function read_tanVelRecon(ncfile)
+    nEdges = UInt8.(ncfile["nEdgesOnEdge"][:]::Vector{Int32})
+    max_n_edges = Int(maximum(nEdges))
+    return read_tanVelRecon(Val(max_n_edges), nEdges, ncfile)
+end
+ 
+function _MPASMesh(ncfile::NCDatasets.NCDataset)
+    voro_mesh = VoronoiMesh(ncfile, false)
+    tanVelRecon = read_tanVelRecon(ncfile)
+    attrib = convert(Dict{String, Union{String, Float64, Int32, Int64}}, ncfile.attrib)
+    return MPASMesh(voro_mesh.cells, voro_mesh.vertices, voro_mesh.edges, tanVelRecon, attrib)
+end
+
+function MPASMeshes.MPASMesh(ncfile::NCDatasets.NCDataset, warn_issues::Bool=true)
+    mesh = _MPASMesh(ncfile)
+    if warn_issues
+        Threads.@spawn VoronoiMeshes.warn_issues(check_mesh($mesh), $mesh)
+    end
+    return mesh
+end
+
+function MPASMeshes.MPASMesh(file_name::String, warn_issues::Bool = true)
+    f = NCDataset(file_name)
+    try
+        MPASMeshes.MPASMesh(f, warn_issues)
+    finally
+        close(f)
+    end
+end
+
+function write_tanVelRecon_data!(ds::NCDataset, tanVelRecon::TangentialVelocityReconstructionThuburn{N, TI, TF}) where {N, TI, TF}
+    ds.dim["maxEdges2"] = N
+
+    defVar(
+        ds, "nEdgesOnEdge", TI.(tanVelRecon.indices.length),
+        ("nEdges",), attrib = [
+            "units" => "-",
+            "long_name" => "Number of edges involved in reconstruction of tangential velocity for an edge."
+        ]
+    )
+
+    defVar(
+        ds, "edgesOnEdge", reinterpret(reshape, TI, tanVelRecon.indices.data),
+        ("maxEdges2", "nEdges"), attrib = [
+            "units" => "-",
+            "long_name" => "IDs of edges involved in reconstruction of tangential velocity for an edge."
+        ]
+    )
+
+    defVar(
+        ds, "weightsOnEdge", reinterpret(reshape, TF, tanVelRecon.weights.data),
+        ("maxEdges2", "nEdges"), attrib = [
+            "units" => "-",
+            "long_name" => "Weights used in reconstruction of tangential velocity for an edge."
+        ]
+    )
+
+    return ds
+end
+
+function save_to_netcdf!(ds::NCDataset, mesh::MPASMesh{S, N, TI}; force3D::Bool=true, write_computed::Bool=true) where {S, N, TI}
+
+    save_to_netcdf!(ds, VoronoiMesh(mesh.cells, mesh.vertices, mesh.edges), force3D=true, write_computed=true)
+
+    defVar(
+        ds, "indexToCellID", TI.(1:mesh.cells.n),
+        ("nCells",), attrib = [
+            "units" => "-",
+            "long_name" => "Mapping from local array index to global cell ID"
+        ]
+    )
+
+    defVar(
+        ds, "indexToVertexID", TI.(1:mesh.vertices.n),
+        ("nVertices",), attrib = [
+            "units" => "-",
+            "long_name" => "Mapping from local array index to global vertex ID"
+        ]
+    )
+
+    defVar(
+        ds, "indexToEdgeID", TI.(1:mesh.edges.n),
+        ("nEdges",), attrib = [
+            "units" => "-",
+            "long_name" => "Mapping from local array index to global edge ID"
+        ]
+    )
+
+    write_tanVelRecon_data!(ds, mesh.tanVelRec)
+
+    for (key, val) in pairs(mesh.attributes)
+        ds.attrib[key] = val
+    end
+
+    return ds
+end
+
+include("precompile_NCDatasetsExt.jl")
+
+end # module
--- src/MPASMeshes.jl
+++ src/MPASMeshes.jl
@@ -1,5 +1,104 @@
 module MPASMeshes
 
-# Write your package code here.
+using Zeros, TensorsLite, VoronoiMeshes, VoronoiOperators
+using Reexport
+
+@reexport using VoronoiMeshes
+export MPASMesh
+
+import Random
+
+const TVRecon{TI, TF} = VoronoiOperators.TangentialVelocityReconstruction{max_Edges2, TI, TF} where {max_Edges2}
+
+struct MPASMesh{OnSphere, max_nEdges, TI, TF, TZ, TVR<:TVRecon{TI, TF}} <: AbstractVoronoiMesh{OnSphere, max_nEdges, TI, TF, TZ}
+    cells::Cells{OnSphere, max_nEdges, TI, TF, TZ}
+    vertices::Vertices{OnSphere, max_nEdges, TI, TF, TZ}
+    edges::Edges{OnSphere, max_nEdges, TI, TF, TZ}
+    tanVelRec::TVR
+    attributes::Dict{String, Union{String, TF, Int32, Int64}}
+
+    function MPASMesh(cells::Cells{OnSphere, max_nEdges, TI, TF, TZ},
+            vertices::Vertices{OnSphere, max_nEdges, TI, TF, TZ},
+            edges::Edges{OnSphere, max_nEdges, TI, TF, TZ},
+            tanVelRec::TVR,
+            attributes::Dict{String, Union{String, TF, Int32, Int64}}
+        ) where {OnSphere, max_nEdges, TI, TF, TZ, TVR<:TVRecon{TI, TF}}
+
+        if !(get_diagram(cells) === get_diagram(vertices) === get_diagram(edges))
+            throw(DimensionMismatch("`Cell`, `Vertices` and `Edges` structs are not based on the same Voronoi diagram"))
+        end
+
+        return new{OnSphere, max_nEdges, TI, TF, TZ, TVR}(cells, vertices, edges, tanVelRec, attributes)
+    end
+end
+
+for N in 6:9
+    for N2 in 10:(2N)
+        precompile(MPASMesh, (Cells{false, N, Int32, Float64, Zero}, Vertices{false, N, Int32, Float64, Zero}, Edges{false, N, Int32, Float64, Zero}, TangentialVelocityReconstructionThuburn{N2, Int32, Float64}, Dict{String, Union{String, Float64, Int32, Int64}}))
+        precompile(MPASMesh, (Cells{true, N, Int32, Float64, Float64}, Vertices{true, N, Int32, Float64, Float64}, Edges{true, N, Int32, Float64, Float64}, TangentialVelocityReconstructionThuburn{N2, Int32, Float64}, Dict{String, Union{String, Float64, Int32, Int64}}))
+    end
+end
+
+function generate_attributes(mesh::AbstractVoronoiMesh{S, N, TI, TF}) where {S, N, TI, TF}
+    attrib = Dict{String, Union{String, TF, Int32, Int64}}()
+
+    attrib["mesh_spec"] = "1.0"
+    attrib["file_id"] = Random.randstring(8)
+
+    if S
+        attrib["on_a_sphere"] = "YES"
+        attrib["sphere_radius"] = mesh.sphere_radius
+        attrib["is_periodic"] = "NO"
+    else
+        attrib["on_a_sphere"] = "NO"
+        attrib["sphere_radius"] = zero(TF)
+        attrib["is_periodic"] = "YES"
+        attrib["x_period"] = mesh.x_period
+        attrib["y_period"] = mesh.y_period
+    end
+
+    return attrib
+end
+
+MPASMesh(mesh::AbstractVoronoiMesh, tanVelRec::TVRecon) = MPASMesh(mesh.cells, mesh.vertices, mesh.edges, tanVelRec, generate_attributes(mesh))
+MPASMesh(mesh::AbstractVoronoiMesh) = MPASMesh(mesh, TangentialVelocityReconstructionThuburn(mesh))
+
+MPASMesh(N::Integer, lx::Real, ly::Real; density::F = x->1, max_iter::Integer = 20000, rtol::Real = 1e-10) where {F} = MPASMesh(VoronoiMesh(N, lx, ly; density = density, max_iter = max_iter, rtol = rtol))
+MPASMesh(points::AbstractVector{<:Vec}, lx::Real, ly::Real; density::F = x->1, max_iter::Integer = 20000, rtol::Real = 1e-10) where {F} = MPASMesh(VoronoiMesh(points, lx, ly; density = density, max_iter = max_iter, rtol = rtol))
+
+function compute_mpas_fields!(mesh::MPASMesh)
+    mesh.cells.area
+    mesh.cells.longitude
+    mesh.cells.latitude
+    mesh.vertices.area
+    mesh.vertices.kiteAreas
+    mesh.vertices.longitude
+    mesh.vertices.latitude
+    mesh.edges.length
+    mesh.edges.lengthDual
+    mesh.edges.angle
+    mesh.edges.longitude
+    mesh.edges.latitude
+    return mesh
+end
+
+function VoronoiMeshes.save(filename, obj::MPASMesh; kwds...)
+    if VoronoiMeshes.is_netcdf_ext(filename)
+        compute_mpas_fields!(obj)
+        VoronoiMeshes.save_to_netcdf(filename, obj; kwds...)
+    else
+        error("Unsupported file extension: $filename")
+    end
+    return nothing
+end
+
+for N in 6:9
+    for N2 in 10:(2N)
+        precompile(compute_mpas_fields!, (MPASMesh{false, N, Int32, Float64, Zeros.Zero, TangentialVelocityReconstructionThuburn{N2, Int32, Float64}},))
+        precompile(compute_mpas_fields!, (MPASMesh{true, N, Int32, Float64, Float64, TangentialVelocityReconstructionThuburn{N2, Int32, Float64}},))
+        precompile(Tuple{typeof(VoronoiMeshes.save), String, MPASMesh{false, N, Int32, Float64, Zeros.Zero, VoronoiOperators.TangentialVelocityReconstructionThuburn{N2, Int32, Float64}}})
+        precompile(Tuple{typeof(VoronoiMeshes.save), String, MPASMesh{true, N, Int32, Float64, Float64}, VoronoiOperators.TangentialVelocityReconstructionThuburn{N2, Int32, Float64}})
+    end
+end
 
 end
